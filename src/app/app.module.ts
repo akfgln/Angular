7727438f0442d0a4d9ce0361e94258c6f9ba2047
akif gülen <akfgln@gmail.com>
2019-12-15 20:09:00 +0100

    single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -44,7 +45,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [ 
+      DishService, 
+      PromotionService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
